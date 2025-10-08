8ff91d25773d779119218b7e91f11bce83c7806b
Weiyang Liu <eulerliu@gmail.com>
2025-10-08 09:53:48 +0800

    Delete inference.sh

--- inference.sh
+++
@@ -1,8 +0,0 @@
-python inference.py \
-    --config_path configs/self_forcing_dmd.yaml \
-    --output_folder path/of/your/videos \
-    --checkpoint_path path/of/your/checkpoint \
-    --data_path vidprom_filtered_extended.txt \
-    --full_budget False \
-    --use_ema
-
